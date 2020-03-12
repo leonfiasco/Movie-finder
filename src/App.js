fe3edb9f5db08eced47eb328e443ce123a950c97
leon Boakye-Adjei <leon_adjei@hotmail.com>
2020-03-12 15:14:03 +0000

    added nav and search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Nav from './components/Nav';
 import './App.css';
+import SearchArea from './components/SearchArea';
 
 function App() {
   return (
     <div className="App">
- 
+        <Nav />
+        <SearchArea />
     </div>
   );
 }
---
+++ src/components/SearchArea.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function SearchArea() {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <section className='col s4 offset-s4'>
+                    <form action=''>
+                        <div className='input-field'>
+                            <input placeholder='Search Movies' type='text'/>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default SearchArea
